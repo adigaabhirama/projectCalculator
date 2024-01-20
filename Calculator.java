2fe47825cb1d2864e29eafaa98d4e6c0f204a286
adigaabhirama <156497450+adigaabhirama@users.noreply.github.com>
2024-01-20 11:52:13 +0530

    My first program the  calculator Haki

---
+++ Calculator.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Simple Calculator with java");
+
+        double num1, num2, result;
+        char operator;
+
+        System.out.print("Enter the first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter an operator for an operation ");
+        operator = scanner.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        num2 = scanner.nextDouble();
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result is : " + result);
+                } else {
+                    System.out.println("Error : Cannot divide by zero.");
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator");
+        }
+
+        scanner.close();
+    }
+}
