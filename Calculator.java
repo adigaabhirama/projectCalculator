b12cf0ef989336c71d0b965ff17a8f2d2079f29f
adigaabhirama <156497450+adigaabhirama@users.noreply.github.com>
2024-02-12 16:37:56 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -18,30 +18,30 @@ class Calculator {
         System.out.println("Enter the second number: ");
         num2 = 12;//scanner.nextDouble();
 
-        switch (operator) {
-            case '+':
+        // switch (operator) {
+        //     case '+':
                 result = num1 + num2;
                 System.out.println("Result: " + result);
-                break;
-            case '-':
-                result = num1 - num2;
-                System.out.println("Result: " + result);
-                break;
-            case '*':
-                result = num1 * num2;
-                System.out.println("Result: " + result);
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                    System.out.println("Result is : " + result);
-                } else {
-                    System.out.println("Error : Cannot divide by zero.");
-                }
-                break;
-            default:
-                System.out.println("Error: Invalid operator");
-        }
+        //         break;
+        //     case '-':
+        //         result = num1 - num2;
+        //         System.out.println("Result: " + result);
+        //         break;
+        //     case '*':
+        //         result = num1 * num2;
+        //         System.out.println("Result: " + result);
+        //         break;
+        //     case '/':
+        //         if (num2 != 0) {
+        //             result = num1 / num2;
+        //             System.out.println("Result is : " + result);
+        //         } else {
+        //             System.out.println("Error : Cannot divide by zero.");
+        //         }
+        //         break;
+        //     default:
+        //         System.out.println("Error: Invalid operator");
+        // }
 
         //scanner.close();
     }
